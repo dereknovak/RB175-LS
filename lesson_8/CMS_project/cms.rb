9fb447b6f99d763c1594576395ae01cd11e21c1a
Derek Novak <derek.novak1@gmail.com>
2024-05-05 22:07:01 -0500

    Restarted CMS project after completign LS202 first 5 lessons

--- lesson_8/CMS_project/cms.rb
+++ lesson_8/CMS_project/cms.rb
@@ -3,67 +3,72 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'redcarpet'
 
-root = File.expand_path('..', __FILE__)
-markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+helpers do
+
+end
+
+def load_file_content(path)
+  content = File.read(path)
+
+  case File.extname(path)
+  when '.txt'
+    headers['Content-Type'] = 'text/plain'
+    content
+  when '.md'
+    headers['Content-Type'] = 'text/html; charset=utf-8'
+    render_markdown(content)
+  end
+end
 
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   markdown.render(text)
 end
 
-def load_file_content(path)
-  content = File.read(path)
-  
-  case File.extname(path)
-  when '.txt'
-    headers['Content-Type'] = 'text/plain'
-    content
-  when '.md'
-    render_markdown(content)
-  end
-end
-
-configure do
-  enable :sessions
-  set :session_secret, SecureRandom.hex(32)
-end
-
-before do
-  @files = Dir.glob(root + '/data/*').map { |file| File.basename(file) }
-end
+root = File.expand_path("..", __FILE__)
 
 get '/' do
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+
   erb :index
 end
 
 get '/:filename' do
   file_path = root + '/data/' + params[:filename]
-  file_name = File.basename(file_path)
-  
+
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+
   if File.exist?(file_path)
     load_file_content(file_path)
   else
-    session[:error] = "#{params[:filename]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist."
     redirect '/'
   end
 end
 
-post '/:filename' do
-  file_path = root + '/data/' + params[:filename]
-  file_name = File.basename(file_path)
-
-  File.write(file_path, params[:content])
-
-  session[:message] = "#{file_name} has been updated."
-  redirect '/'
-end
-
 get '/:filename/edit' do
   file_path = root + '/data/' + params[:filename]
-  file_name = File.basename(file_path)
 
   @filename = params[:filename]
   @content = File.read(file_path)
 
   erb :edit
+end
+
+post '/:filename' do
+  file_path = root + '/data/' + params[:filename]
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect '/'
 end
